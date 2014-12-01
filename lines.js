223d7bf7f8cf6c65462de8d17763ce6b1b089f48
kdog <kdoglinux@gmail.com>
2014-11-30 16:58:38 -0800

    committing changes

--- lines.js
+++ lines.js
@@ -1,18 +1,20 @@
 var through = require('through')
 var split   = require('split')
 
-var tr     = through(write)
 var lcount = 0
-function write(buf){
+var tr     = through(function(buf){
     var line = buf.toString()
     this.queue(lcount % 2 === 0
 	       ? line.toLowerCase() + '\n'
 	       : line.toUpperCase() + '\n'
     )
     lcount++
-}
+})
 
-process.stdin.pipe(split()).pipe(tr).pipe(process.stdout)
+process.stdin
+    .pipe(split())
+    .pipe(tr)
+    .Pipe(process.stdout)
 
 
 
